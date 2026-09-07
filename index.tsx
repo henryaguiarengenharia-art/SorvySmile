b09049ceb8e84475a280cf16874ece13fead9cb7
henryaguiarengenharia-art <henry.aguiar.engenharia@gmail.com>
2026-09-07 20:33:06 -0300

    Expose Smile Map V4 demo route

--- index.tsx
+++ index.tsx
@@ -1,20 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AppErrorBoundary } from "./components/AppErrorBoundary";
+import { SmileMapV4Demo } from "./components/SmileMapV4Demo";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { AppErrorBoundary } from './components/AppErrorBoundary';
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+
+const isSmileMapV4Demo = window.location.pathname === "/demo/smile-map-v4";
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <AppErrorBoundary>
-      <App />
+      {isSmileMapV4Demo ? <SmileMapV4Demo /> : <App />}
     </AppErrorBoundary>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
